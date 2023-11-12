54133476f162cd6c2f56e6cb8b1f15f6b6247aaf
andrezambroni <andrezambroni@gmail.com>
2023-11-12 14:29:26 -0300

    react4

---
+++ react-santander/3/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import MinhaLista from "./components/MinhaLista";
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Listas do React</h1>
+      <MinhaLista/>
+
+    </div>
+  )
+}
+
+
