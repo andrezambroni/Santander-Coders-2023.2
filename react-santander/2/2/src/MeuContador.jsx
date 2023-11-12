786588bfd207a7efd6606b04a408a25706388d90
andrezambroni <andrezambroni@gmail.com>
2023-11-12 13:25:43 -0300

    react3

--- react-santander/2/2/src/MeuContador.jsx
+++ react-santander/2/2/src/MeuContador.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 
 export default function MeuContador() {
+  const [contador, setContador] = useState(0);
 
-    const [contador, setContador] = useState(0)
+  function aumentar() {
+    setContador(contador + 1);
+  }
 
-    
+  function diminuir() {
+    setContador(contador - 1);
+  }
 
-    function aumentar(){
-        setContador(contador + 1)
-    }
-    
-    
+  if (contador > 5) {
+    return (
+      <div>
+        <h1>contador maior que 5</h1>
+        <button onClick={aumentar}>aumentar</button>
+        <button onClick={diminuir}>diminuir</button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>Meu Contador</h1>
-      <h3>{contador}</h3>
-      <button onClick={aumentar}>+</button>
+      <h1>Meu Contador: {contador}</h1>
+      <button onClick={aumentar}>aumentar</button>
+        <button onClick={diminuir}>diminuir</button>
     </div>
   );
 }
